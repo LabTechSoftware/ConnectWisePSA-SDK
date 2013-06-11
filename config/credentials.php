fb4c36d793ce7bf9436ed1d62cc3cdad109da49d
Marc Seiler <mseiler@gmail.com>
2013-06-11 08:51:51 -0400

    Added the credentials.php to the config folder.

---
+++ config/credentials.php
@@ -0,0 +1,15 @@
+<?php return array(
+
+    // ConnectWise API Domain
+    'domain' => '',
+
+    // ConnectWise Company
+    'company' => '',
+
+    // ConnectWise Integrator Username
+    'cwusername' => '',
+
+    // ConnectWise Integrator Password
+    'cwpass' => '',
+
+);
