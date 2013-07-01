67b7a4eaa9215a6dacc45574bbef3c27bf358764
The <azavala@labtechsoftware.com>
2013-07-01 16:31:26 -0400

    Ops, forgot to do a pull before previous commit. Credentials file changed back.

--- config/credentials.php
+++ config/credentials.php
@@ -1,18 +1,6 @@
 <?php return array(
 
     // ConnectWise API Domain
-<<<<<<< HEAD
-    'domain' => 'test.connectwise.com',
-
-    // ConnectWise Company
-    'company' => 'LabTech',
-
-    // ConnectWise Integrator Username
-    'cwusername' => 'webdev',
-
-    // ConnectWise Integrator Password
-    'cwpass' => 'webdev',
-=======
     'domain' => '',
 
     // ConnectWise Company
@@ -23,6 +11,5 @@
 
     // ConnectWise Integrator Password
     'cwpass' => '',
->>>>>>> fb4c36d793ce7bf9436ed1d62cc3cdad109da49d
 
 );
