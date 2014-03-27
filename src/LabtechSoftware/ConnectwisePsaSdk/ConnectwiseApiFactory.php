6343eb54db5ba92d71c11b02d82987bcff3f39ed
The <azavala@labtechsoftware.com>
2014-03-27 11:11:39 -0400

    Removing old code, fixing tags.

--- src/LabtechSoftware/ConnectwisePsaSdk/ConnectwiseApiFactory.php
+++ src/LabtechSoftware/ConnectwisePsaSdk/ConnectwiseApiFactory.php
@@ -41,30 +41,4 @@ class ConnectwiseApiFactory
 
         return new SoapApiRequester($soap, $cl);
     }
-
-
-/*    private $methodPrefix = 'make';
-    private $methodToCall = '';
-
-    public function __call($methodName, $args)
-    {
-        $this->setMethodName($methodName);
-
-        if (method_exists($this, $this->methodToCall) === false) {
-            throw new Exception('Unknown Method.');
-        }
-
-        return $this->runMethod($this->methodToCall, $args);
-    }
-
-    private function runMethod($class, $args)
-    {
-        return call_user_func_array(array($this, $class), $args);
-    }
-
-    private function setMethodName($method)
-    {
-        $this->methodToCall = $this->methodPrefix . ucfirst($method);
-    }*/
-
 }
